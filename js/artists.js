9f479baf7ee969b27f94df38f4322cf0cdcf5719
Tevale92 <daviddemers92@gmail.com>
2025-02-02 17:04:32 -0500

    First upload of project, v6 without any javascript

---
+++ js/artists.js
@@ -0,0 +1,22 @@
+const artists1 = ["Noah Kahan", "Melanie Martinez", "Dominic Fike", "Lil Tjay", "Skepta", 
+    "Teddy Swims", "Two Door Cinema Club", "Mochakk", "Bladee", "Sleater-Kinney", 
+    "Mariah the Scientist", "Tezzo Touchdown", "Romy", "Overmono", "Blonde Redhead", 
+    "Arlo Parks", "Fridayy", "Byron Messia", "Jyoty", "The Japanese House", "Avalon Emerson", 
+    "Mimi Webb", "Wild Rivers", "Mannequin Pussy", "Billianne", "DJ Minx", "Cam Kahin", 
+    "Myst Milano", "Leonie Gray", "Alias"];
+
+const artists2 = ["Green Day", "The Smashing Pumpkins", "Martin Garrix", "Reneé Rapp", 
+    "Labrinth", "T-Pain", "Rancid", "Gryffin", "Elderbrook", "Denzel Curry", "Uncle Waffles", 
+    "Talk", "Chappell Roan", "Brittany Howard", "Michaël Brun", "CRi", "Olivia Dean", 
+    "The Linda Lindas", "Sofia Kourtesis", "Libianca", "Irène Drésel", "Lola Young", 
+    "Nonso Amadi", "Soul Clap", "Mariana Gueza", "Sinca", "Minoe", "No Waves", "DVTR"];
+
+const artists3 = ["SZA", "Hozier", "Justice", "Jungle", "Hamza", "Still Woozy", "Alvvays", 
+    "Stephen Sanchez", "Raye", "Tyla", "Ayra Starr", "Kevin Abstract", "Mau P", 
+    "Briston Maroney", "Folamour", "Amyl and The Sniffers", "Chris Stussy", "ElGrandetoto", 
+    "ElYanna", "Ash", "Confidence Man", "DIIV", "Clay and Friends", "Planet Giza", 
+    "The Blue Stones", "Model Man", "Marina Trench", "Ya Cetidon", "Cadence Weapon"];
+
+const days = ["Friday August Second", "Saturday August Third", "Sunday August Fourth"]
+
+document
